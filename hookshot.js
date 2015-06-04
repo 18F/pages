dd52b1b518e76b61374eba106a7041ddcbf0e305
Mike Bland <michael.bland@gsa.gov>
2015-06-04 15:23:23 -0400

    Support pages-staging.18f.gov
    Closes #11.

--- hookshot.js
+++ hookshot.js
@@ -26,8 +26,6 @@ if (!(port && home && rbenv)) {
   process.exit(1);
 }
 
-var REPO_HOME = path.join(home, "pages-repos");
-var DEST_DIR = path.join(home, "pages-generated");
 var GIT = path.join("/", "usr", "bin", "git");
 var BUNDLER = path.join(rbenv, "shims", "bundle");
 var JEKYLL = path.join(rbenv, "shims", "jekyll");
@@ -63,17 +61,23 @@ BuildLogger.prototype.error = function() {
 // to the algorithm. All other methods are "states" of the algorithm/state
 // machine that are executed asynchronously via callbacks.
 //
+// repo_dir: directory containing locally-cloned Pages repositories
 // repo_name: name of the repo belonging to the 18F GitHub organization
+// dest_dir: path to the destination directory for published sites
 // site_path: path to the repo on the local machine
+// branch: the branch to publish
 // build_logger: BuildLogger instance
 // done_callback: callback triggered when the algorithm exits; takes a single
 //   `err` argument which will be nil on success, and an error string on
 //   failure
-function SiteBuilder(repo_name, site_path, build_logger, done_callback) {
+function SiteBuilder(repo_dir, repo_name, dest_dir, site_path, branch,
+  build_logger, done_callback) {
+  this.repo_dir = repo_dir;
   this.repo_name = repo_name;
   this.site_path = site_path;
+  this.branch = branch;
   this.logger = build_logger;
-  this.build_destination = path.join(DEST_DIR, repo_name);
+  this.build_destination = path.join(dest_dir, repo_name);
   this.done = done_callback;
 
   this.spawn = function(path, args, next) {
@@ -110,8 +114,8 @@ SiteBuilder.prototype.clone_repo = function() {
   this.logger.log("cloning", this.repo_name, "into", this.site_path);
 
   var clone_addr = "git@github.com:18F/" + this.repo_name + ".git";
-  var clone_args = ["clone", clone_addr, "--branch", "18f-pages"];
-  var clone_opts = {cwd: REPO_HOME, stdio: 'inherit'};
+  var clone_args = ["clone", clone_addr, "--branch", this.branch];
+  var clone_opts = {cwd: this.repo_dir, stdio: 'inherit'};
   var that = this;
 
   spawn(GIT, clone_args, clone_opts).on('close', function(code) {
@@ -151,9 +155,10 @@ SiteBuilder.prototype.jekyll_build = function() {
   this.spawn(jekyll, args, function() { that.done(null); });
 }
 
-hookshot('refs/heads/18f-pages', function(info) {
+function launch_builder(info, dest_dir, repo_dir) {
   var repo_name = info.repository.name;
-  var site_path = path.join(REPO_HOME, repo_name);
+  var branch = info.ref.split('/').pop();
+  var site_path = path.join(repo_dir, repo_name);
   var commit = info.head_commit;
   var build_log = site_path + '.log';
   var logger = new BuildLogger(build_log);
@@ -162,8 +167,8 @@ hookshot('refs/heads/18f-pages', function(info) {
   logger.log('timestamp:', commit.timestamp);
   logger.log('committer:', commit.committer.email);
 
-  var builder = new SiteBuilder(repo_name, site_path, logger,
-    function(err) {
+  var builder = new SiteBuilder(repo_dir, repo_name, dest_dir, site_path,
+    branch, logger, function(err) {
       if (err !== null) {
         logger.error(err);
         logger.error(repo_name + ': build failed');
@@ -173,7 +178,7 @@ hookshot('refs/heads/18f-pages', function(info) {
 
       // Provides https://pages.18f.gov/REPO-NAME/build.log as an indicator of
       // latest status.
-      var new_log_path = path.join(DEST_DIR, repo_name, 'build.log');
+      var new_log_path = path.join(dest_dir, repo_name, 'build.log');
       fs.rename(build_log, new_log_path, function(err) {
         if (err !== null) {
           console.error('Error moving build log from', build_log, 'to',
@@ -182,6 +187,20 @@ hookshot('refs/heads/18f-pages', function(info) {
       });
     });
   builder.build();
-}).listen(port);
+}
+
+var webhook = hookshot('refs/heads/18f-pages', function(info) {
+  launch_builder(info,
+    path.join(home, "pages-generated"),
+    path.join(home, "pages-repos"));
+});
+
+webhook.on('refs/heads/18f-pages-staging', function(info) {
+  launch_builder(info,
+    path.join(home, "pages-staging"),
+    path.join(home, "pages-repos-staging"));
+});
+
+webhook.listen(port);
 
 console.log("18F pages: listening on port " + port);
