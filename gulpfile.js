db9e1a166b45c6cfc2b07a3cea614cba248fbb49
Mike Bland <michael.bland@gsa.gov>
2015-08-18 13:11:40 -0400

    Install basic NodeJS build, test, and lint configs

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+var jshint = require('gulp-jshint');
+
+gulp.task('test', function() {
+  return gulp.src('./_test/*_test.js', {read: false})
+    // Reporters:
+    // https://github.com/mochajs/mocha/blob/master/lib/reporters/index.js
+    .pipe(mocha({reporter: 'spec'}));
+});
+
+gulp.task('lint', function() {
+  return gulp.src(['./hookshot.js', './_test/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
