ae36b3c18addecab3318b5637810570ebb6abcfd
Mike Bland <michael.bland@gsa.gov>
2015-12-01 15:44:57 -0500

    Make it moar functional at @arowla's suggestion

--- _scripts/all_pages.rb
+++ _scripts/all_pages.rb
@@ -12,12 +12,10 @@ end
 access_token = File.read TOKEN_PATH
 Octokit.auto_paginate = true
 client = Octokit::Client.new access_token: access_token
-repos = client.org_repos '18F'
+repos = client.org_repos('18F').collect { |repo| repo.full_name }
 
-pages_repos = repos.map do |repo|
-  repo_name = repo.full_name
-  branches = client.branches(repo_name).map { |b| b.name }
-  repo_name if branches.include? '18f-pages'
-end.compact.sort
+pages_repos = repos.select do |name|
+  client.branches(name).any? { |b| b.name == '18f-pages' }
+end.sort
 
 pages_repos.each { |repo| puts repo }
